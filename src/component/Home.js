5eda0a355044e201769a954d980a47de6f16b470
fuadmoin <fuadamoin@gmail.com>
2023-04-18 19:46:27 +0300

    Fetch rocket data.

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,33 +1,42 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { fetchRockets } from '../redux/rocket/rocketSlice';
+import React from 'react';
+import place from '../img/planetLogo.png';
 import './home.css';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rocket.rockets);
-  const status = useSelector((state) => state.rocket);
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
+  const arr = [
+    {
+      name: 'Falcon1',
+      description: 'Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009',
+      img: place,
+    },
+    {
+      name: 'Falcon2',
+      description: 'Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009',
+      img: place,
+    },
+    {
+      name: 'Falcon3',
+      description: 'Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009 Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009 Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009 Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009',
+      img: place,
+    },
+    {
+      name: 'Falcon',
+      description: 'Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009',
+      img: place,
+    },
+  ];
   return (
     <div>
-      {
-
-      !status.loading && rockets.length ? (
-
-        rockets.map((rocket) => (
-          <div key={rocket.id} className="rocketContainer">
-            <img className="rocketImage" src={rocket.flickr_images} alt="rocket" />
-            <div className="detailContainer" key={rocket.rocket_name}>
-              <h1>{rocket.rocket_name}</h1>
-              <p>{rocket.description}</p>
-              <button type="button">Reserve Rocket</button>
-            </div>
+      {arr.map((rocket) => (
+        <div key={rocket.name} className="rocketContainer">
+          <img className="rocketImage" src={rocket.img} alt="rocket" />
+          <div className="detailContainer" key={rocket.name}>
+            <h1>{rocket.name}</h1>
+            <p>{rocket.description}</p>
+            <button type="button">Reserve Rocket</button>
           </div>
-        ))) : null
-
-    }
+        </div>
+      ))}
 
     </div>
   );
