f518dabcfd9a56429dd0c2b74be72d2b6c805ebb
fuadmoin <fuadamoin@gmail.com>
2023-04-18 23:38:02 +0300

    Make book functionality.

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
-import { fetchRockets } from '../redux/rocket/rocketSlice';
+import { fetchRockets, bookRocket } from '../redux/rocket/rocketSlice';
+import RocketBooking from './RocketBooking';
 import './home.css';
 
 const Home = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocket.rockets);
   const status = useSelector((state) => state.rocket);
+  const handleRocketBooking = (id) => {
+    dispatch(bookRocket(id));
+  };
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
@@ -22,7 +26,11 @@ const Home = () => {
             <div className="detailContainer" key={rocket.rocket_name}>
               <h1>{rocket.rocket_name}</h1>
               <p>{rocket.description}</p>
-              <button type="button">Reserve Rocket</button>
+              <RocketBooking
+                bookRocket={handleRocketBooking}
+                id={rocket.id}
+                status={rocket.reserved}
+              />
             </div>
           </div>
         ))) : null
---
+++ src/component/RocketBooking.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './home.css';
+import PropTypes from 'prop-types';
+
+const RocketBooking = ({ bookRocket, id, status }) => {
+  const buttonStatus = {};
+  if (status) {
+    buttonStatus.backgroundColor = 'white';
+    buttonStatus.color = 'grey';
+    buttonStatus.width = '150px';
+    buttonStatus.borderBottom = '1px solid black';
+    buttonStatus.borderRight = '1px solid black';
+    buttonStatus.borderLeft = '1px solid grey';
+    buttonStatus.borderTop = '1px solid grey';
+  }
+  return (
+    <button
+      style={buttonStatus}
+      className="btnReserve"
+      type="button"
+      onClick={
+          () => bookRocket(id)
+      }
+    >
+      {status ? 'Cancel Reservation' : 'Reserve'}
+    </button>
+  );
+};
+
+RocketBooking.propTypes = {
+  bookRocket: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
+};
+
+export default RocketBooking;
