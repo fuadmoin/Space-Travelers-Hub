19d61adf0eb4e3e844433bc4346bca967d61100b
Yidne <yamiyekn@gmail.com>
2023-04-18 16:30:41 +0300

    Fix linters.

--- src/redux/mission/missionSlice.js
+++ src/redux/mission/missionSlice.js
@@ -1,22 +1,21 @@
-import { createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [];
 
 export const missionSlice = createSlice({
-    name: 'mission',
-    initialState,
-    reducers: {
-      join: (state, action) => {
-        state.push(action.payload);
-      },
-      leave: (state, action) => {
-        const Status = [...state];
-        state.splice(0, state.length);
-        state.push(...Status.filter((status) => status.item_id !== action.payload));
-      },
+  name: 'mission',
+  initialState,
+  reducers: {
+    join: (state, action) => {
+      state.push(action.payload);
     },
-  });
-  
-  export const { join, leave } = missionSlice.actions;
-  export default missionSlice.reducer;
-  
+    leave: (state, action) => {
+      const Status = [...state];
+      state.splice(0, state.length);
+      state.push(...Status.filter((status) => status.item_id !== action.payload));
+    },
+  },
+});
+
+export const { join, leave } = missionSlice.actions;
+export default missionSlice.reducer;
