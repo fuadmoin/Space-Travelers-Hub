a999006c01e283b6d4b638b68abfc1493ece07ca
Yidne <yamiyekn@gmail.com>
2023-04-19 13:41:22 +0300

    Display mission klist.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rocketReducer from './rocket/rocketSlcie';
+import rocketReducer from './rocket/rocketSlice';
 import missionReducer from './mission/missionSlice';
 
 const store = configureStore({
   reducer: {
     rocket: rocketReducer,
-    mission: missionReducer,
+    missions: missionReducer,
   },
 });
 
