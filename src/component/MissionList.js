1464b30d3b10a1ef65768e6bd90171074dd91f68
Yidne <yamiyekn@gmail.com>
2023-04-19 14:36:01 +0300

    Add join functionality.

--- src/component/MissionList.js
+++ src/component/MissionList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/mission/missionSlice';
 import styles from './modules/MissionList.module.css';
 
 function MissionLists({ mission }) {
+  const dispatch = useDispatch();
+  const handleJoin = (id) => {
+    dispatch(joinMission(id));
+  };
   return (
     <tr className={styles.tr}>
       <th className={styles.th}>{mission.mission_name}</th>
@@ -10,7 +16,7 @@ function MissionLists({ mission }) {
         {mission.description}
       </td>
       <td className={styles.td}><button type="button">Not a member</button></td>
-      <td className={styles.td}><button type="button">Join mission</button></td>
+      <td className={styles.td}><button type="button" onClick={() => handleJoin(mission.mission_id)}>Join mission</button></td>
     </tr>
 
   );
@@ -18,7 +24,7 @@ function MissionLists({ mission }) {
 
 MissionLists.propTypes = {
   mission: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    mission_id: PropTypes.string.isRequired,
     mission_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     joined: PropTypes.bool,
