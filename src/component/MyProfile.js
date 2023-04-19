d1edb53613b0b48fac1afbae3ba16a567b3f5635
fuadmoin <fuadamoin@gmail.com>
2023-04-19 13:41:45 +0300

    Switch badges.

--- src/component/MyProfile.js
+++ src/component/MyProfile.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux/es/exports';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rocket.rockets);
+  const filterdRockets = rockets.filter((rocket) => rocket.reserved === true);
+
+  return (
+    <div>{filterdRockets.length}</div>
+  );
+};
 
 export default MyProfile;
